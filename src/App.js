3490183896b7145751c72ca9620ffe01132a5a81
nkshariharan <nkshariharan17@gmail.com>
2022-04-01 12:55:07 +0530

    app.js and index.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,34 @@
-import logo from './logo.svg';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 import './App.css';
+import AdminDashboard from './components/adminComponent/AdminDashboard';
+import AdminLogin from './components/adminComponent/AdminLogin';
+import CreateNewTask from './components/adminComponent/CreateNewTask';
+import EditTask from './components/adminComponent/EditTask';
+import StudentTask from './components/adminComponent/StudentTask';
+import UpdateStudentTask from './components/adminComponent/UpdateStudentTask';
+import SubmitForm from './components/userComponent/SubmitForm';
+import UserDashboard from './components/userComponent/UserDashboard';
+import Login from './pages/Login';
+import Register from './pages/Register';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <BrowserRouter>
+    <Routes>
+      <Route path="/" element={<Login />} />
+      <Route path="/adminlogin" element={<AdminLogin />} />
+      <Route path="/register" element={<Register />} />
+
+      <Route path="/admindashboard" element={<AdminDashboard />} />
+      <Route path="/add-task" element={<CreateNewTask />} />
+      <Route path="/edit-task/:id" element={<EditTask />} />
+      <Route path="/studenttask" element={<StudentTask />} />
+      <Route path="/studenttask/:id" element={<UpdateStudentTask />} />
+
+      <Route path="/dashboard" element={<UserDashboard />} />
+      <Route path="/submitform" element={<SubmitForm />} />
+    </Routes>
+    </BrowserRouter>
   );
 }
 
